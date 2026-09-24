d40f3dc89673bbd089de28dabc7311fe73690318
ALI HAMZA SINDHU <asindh096@gmail.com>
2026-09-24 15:17:56 +0500

    Input

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -14,7 +14,6 @@ import FreeShippingBar from "../components/cart/FreeShippingBar";
 import FramedImage from "../components/ui/FramedImage";
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
-
 import { Card } from "../components/ui/Card";
 
 const CartPage = () => {
