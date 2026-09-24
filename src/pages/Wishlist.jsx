634cd25aff2f616579001cf309c8e191739899c2
ALI HAMZA SINDHU <asindh096@gmail.com>
2026-09-24 15:20:57 +0500

    Button

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -9,7 +9,7 @@ import { addToCartThunk as addToCart } from "../features/cart/cartThunks";
 import { openCart } from "../store/slice/Uislice";
 import PageHeader from "../components/layout/PageHeader";
 import SampleProduct from "../components/SampleProduct";
-import { Button } from "../components/ui/button";
+import { Button } from "../components/ui/Button";
 import { getProductOptions } from "../utils/productOptions";
 
 const Wishlist = () => {
