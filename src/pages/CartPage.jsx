29791978874a992ce23ac3c740c8643fee744bfa
ALI HAMZA SINDHU <asindh096@gmail.com>
2026-09-24 15:16:19 +0500

    Input

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -13,7 +13,6 @@ import PageHeader from "../components/layout/PageHeader";
 import FreeShippingBar from "../components/cart/FreeShippingBar";
 import FramedImage from "../components/ui/FramedImage";
 import { Button } from "../components/ui/Button";
-
 import { Input } from "../components/ui/Input";
 
 import { Card } from "../components/ui/Card";
